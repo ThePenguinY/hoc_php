c98da59e5fb9e5cae0b00e9f2c667bd9dbd38d71
ThePenguinY <hoanganhn0407@gmail.com>
2023-12-08 09:24:04 +0700

    Hoàng Anh

--- BTL-PHP/index.php
+++ BTL-PHP/index.php
@@ -1,31 +1,63 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Đăng nhập</title>
-    <link rel="stylesheet" href="style.css">
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Ribit Store - Bridal Wedding Dress</title>
+   <link rel="stylesheet" href="./styleMainMenu.css">
 </head>
 <body>
-    <div class="center">
-        <h1>Đăng nhập</h1>
-        <form method="post">
-            <div class="txt_field">
-                <input type="text" required>
-                <span></span>
-                <label>Username</label>
-            </div>
-            <div class="txt_field">
-                <input type="password" required>
-                <span></span>
-                <label>Password</label>
-            </div>
-            <div class="pass">Forgot Password?</div>
-            <input type="submit" value="Login">
-            <div class="signup_link" >
-                Not a member? <a href="#">Signup</a>
-            </div>
-        </form>
-    </div>
+   <!--Main Menu-->
+
+   <section>
+      <header>
+      <div class="circle"></div>
+      <div class="circles"></div>
+      <a href="#" class="logo">
+         Ribit
+         <span>Store</span>
+      </a>
+      <ul>
+         <li>
+            <a href="#">Đầm ngắn</a>
+         </li>
+         <li>
+            <a href="#">Đầm dài</a>
+         </li>
+         <li>
+            <a href="#">Đầm ren</a>
+         </li>
+         <li>
+            <a href="#">Áo dài</a>
+         </li>
+         <li>
+            <a href="#">Đầm màu</a>
+         </li>
+         <li>
+            <a href="#">Phụ kiện</a>
+         </li>
+         <li>
+            <a href="#">About</a>
+         </li>
+      </ul>
+      <p>Đăng nhập</p>
+      </header>
+      <div class="texts">
+         <h1>Ribit
+            <span>Store</span></h1>
+         <p>Để mang lại sự thuận tiện cho Quý Khách. Xin vui lòng gọi trực tiếp 
+            cho nhân viên tư vấn hoặc đến cửa hàng để được hỗ trợ nhanh nhất.</p>
+      </div>
+      <div class="background_img">
+         <img src="https://as2.ftcdn.net/v2/jpg/02/82/10/69/1000_F_282106997_cu2uB4YSmmTjXagqdlTjQ0EOYthhzdZR.jpg" alt="Image not found">
+      </div>
+
+      <div class = "btn">
+         <p class="read">More</p>
+         <p>Thuê</p>
+      </div>
+   </section>
+
+   <!-- content -->
 </body>
 </html>
